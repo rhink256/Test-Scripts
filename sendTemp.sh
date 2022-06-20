72168171e4238f8417fc2a500bb78bad280b0623
rhink256 <rhink@vt.edu>
2022-06-20 17:20:22 -0400

    Update sendTemp.sh

--- sendTemp.sh
+++ sendTemp.sh
@@ -12,7 +12,5 @@ echo "Humidity: $humidity";
 echo "Pressure: $pressure";
 
 echo 'Content-Type: application/json;charset=UTF-8'
-#echo "Content-Type"
-
 
 curl -X PUT localhost:8081/temp/ -H 'Content-Type: application/json;charset=UTF-8' -d "{"\""celsius"\"":$celsius, "\""humidity"\"":$humidity, "\""pressure"\"":$pressure, "\""illuminance"\"":12, "\""expectedUpdateRateSeconds"\"":300}"
